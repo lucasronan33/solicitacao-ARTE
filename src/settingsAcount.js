24ec18dc30ab316c7b8830126be6d1c773a65047
Lucas Torres <lucasronan33@gmail.com>
2025-02-25 17:18:09 -0300

    estilos profile

--- src/settingsAcount.js
+++ src/settingsAcount.js
@@ -9,9 +9,11 @@ for (const input of senhaInput) {
         if (inputSenha.type == 'password') {
             inputSenha.type = 'text'
             img.src = './img/olho-aberto.png'
+            img.alt = 'Ocultar senha'
         } else {
             inputSenha.type = 'password'
             img.src = './img/olho-fechado.png'
+            img.alt = 'Mostrar senha'
         }
     })
 }
