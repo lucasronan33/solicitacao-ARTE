23eff745e914c44ccad16fbf1a1a1b691fb914c7
Lucas Torres <lucasronan33@gmail.com>
2025-03-07 17:18:38 -0300

    testes logica inputContato

--- src/inputContato.js
+++ src/inputContato.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputCttComercial = document.getElementById('ctt-comercial')
 
 
-    inputCttComercial.addEventListener('input', (event) => {
+    inputCttComercial.addEventListener('input', function (event) {
 
         let valorInput = inputCttComercial.value
         let valor = inputCttComercial.value.replace(/\D/g, '');
@@ -12,20 +12,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
         console.log(valorInput.length);
         console.log(valorInput);
-        if (valorInput.length >= 0 && valorInput.length <= 4) {
-            console.log(inputCttComercial.value);
-            console.log(inputCttComercial);
 
-            inputCttComercial.value = '+55 (' + valorInput
-        }
-        if (valorInput.length >= 7 && valorInput.length <= 11) {
-            inputCttComercial.value = valorInput + ') 9 '
-        }
-        if (valorInput.length >= 15 && valorInput.length <= 15) {
-            inputCttComercial.value = valorInput + '-'
-        }
+        this.addEventListener('keypress', function (e) {
+            console.log('e:', e);
+            console.log('e.key:', e.key);
 
-        console.log('valorInput', valorInput);
+            if (e.key !== 'backspace') {
+                if (valorInput.length >= 0 && valorInput.length <= 4) {
+                    console.log(inputCttComercial.value);
+                    console.log(inputCttComercial);
 
+                    inputCttComercial.value = '+55 (' + valorInput
+                }
+                if (valorInput.length >= 7 && valorInput.length <= 11) {
+                    inputCttComercial.value = valorInput + ') 9 '
+                }
+                if (valorInput.length >= 15 && valorInput.length <= 15) {
+                    inputCttComercial.value = valorInput + '-'
+                }
+
+                console.log('valorInput', valorInput);
+            }
+        })
     })
 })
